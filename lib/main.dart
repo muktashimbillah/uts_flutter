31b396db3c69617b1b9e04de757d44aa6e8eb907
Muktashim Billah <muktasim.elfatih@gmail.com>
2023-11-02 14:15:24 +0700

    tria

--- lib/main.dart
+++ lib/main.dart
@@ -12,55 +12,103 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false, // Menghilangkan label debug
       home: Scaffold(
-          appBar: AppBar(
-            leading: Icon(Icons.menu),
-            title: Text("Pencarian"),
-            backgroundColor: Colors.orange,
-          ),
           body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 70),
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(top: 30),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 10),
-                        child: Text(
-                          "Jambi",
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      ),
-                      Icon(Icons.all_inclusive_sharp),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: Text(
-                          "Indonesia",
-                          style: TextStyle(fontSize: 18),
-                        ),
-                      )
-                    ],
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: "Masukkan kata",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.horizontal(
-                            left: Radius.circular(50),
-                            right: Radius.circular(50)),
-                      ),
-                      contentPadding: EdgeInsets.symmetric(horizontal: 18),
-                    ),
-                  )
-                ],
+        padding: const EdgeInsets.symmetric(horizontal: 70),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 30),
               ),
-            ),
-          )),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  margin: EdgeInsets.only(top: 10, bottom: 5),
+                  child: const Text(
+                    "DevRnz",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 30),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  margin: EdgeInsets.only(top: 10, bottom: 5),
+                  child: const Text(
+                    "Email Id",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
+                  ),
+                ),
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  hintText: "Masukkan Email",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(10), right: Radius.circular(10)),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 18),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  margin: EdgeInsets.only(top: 10, bottom: 5),
+                  child: const Text(
+                    "Password",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w300),
+                  ),
+                ),
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  hintText: "Masukkan Password",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.horizontal(
+                        left: Radius.circular(10), right: Radius.circular(10)),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 18),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 45),
+                    backgroundColor: Colors.orange),
+                child: Text("Login"),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                  ),
+                  child: Text("Forgot Password"),
+                ),
+              ),
+              Divider(
+                thickness: 1,
+                color: Colors.black,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 40),
+                ),
+                child: Text("Login dengan Facebook"),
+              ),
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
